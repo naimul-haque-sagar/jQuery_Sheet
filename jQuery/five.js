fbd3b52384fb48804888b4101dc7cac86f4a1d35
naimul-haque-sagar <naimulhaquesagar@gmail.com>
2020-08-09 11:59:05 +0600

    file added

--- jQuery/five.js
+++ jQuery/five.js
@@ -1,3 +1,34 @@
+
+$("#button-a").click(() => {
+    $("#div1").delay(5000).fadeOut();
+    $("#div2").fadeOut("slow");
+    $("#div3").fadeOut(3000);
+});
+
+$("#button-b").click(() => {
+    $("#div1").fadeToggle();
+    $("#div2").fadeToggle("slow");
+    $("#div3").fadeToggle(3000);
+});
+
+$("#button-c").click(() => {
+    $("#div1").fadeTo("slow", 0.3);
+    $("#div2").fadeTo("slow", 0.4);
+    $("#div3").fadeTo("slow", 0.5);
+});
+
+$("#click").click(() => {
+    $("#panel").slideDown("slow");
+});
+
+$("#click-1").click(() => {
+    $("#panel-1").slideUp("slow");
+});
+
+$("#click-2").click(() => {
+    $("#panel-1").slideToggle("slow");
+});
+
 $(document).ready(() => {
     $("#hide").click(() => {
         $("p").hide();
@@ -7,7 +38,7 @@ $(document).ready(() => {
 $(document).ready(() => {
     $("#show").click(() => {
         $("p").show();
-    });    
+    });
 });
 
 $(document).ready(() => {
@@ -19,16 +50,15 @@ $(document).ready(() => {
 $(document).ready(() => {
     $("#show1").click(() => {
         $("#par").show(1500);
-    });    
+    });
 });
 
-$("#show2").click(()=>{
+$("#show2").click(() => {
     $("#par").toggle();
 });
 
-$("#buttony").click(()=>{
-    $("#par").fadeIn(3000);
+$("button").click(function () {
+    $(".div-x").animate({ left: '450px' });
 });
-
 
 
